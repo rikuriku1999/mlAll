00c9a1f994421ec6e3110d0be3dd68a449318914
rikuriku1999 <58874178+rikuriku1999@users.noreply.github.com>
2022-11-16 15:50:09 +0900

    Update ml_all_0913.py

--- ml_all/ml_all_0913.py
+++ ml_all/ml_all_0913.py
@@ -538,7 +538,7 @@ for l in l_list:
     smtp_host = 'smtp.gmail.com'
     smtp_port = 465
     username = 'kalashnikova1120@gmail.com'
-    password = 'nfjaizuqmrpspvti'
+    password = ''
     from_address = 'kalashnikova1120@gmail.com'
     to_address = 'kalashnikova1120@gmail.com'
     subject = 'test subject'
